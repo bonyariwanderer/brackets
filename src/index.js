a0df6912cde96ce49e330354b7b2de2dc38dadbb
Julia Bazaeva <truckfulofcandy@gmail.com>
2021-08-30 18:15:55 +0700

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];  
+  const BRACKETS_PAIR = {};
+  bracketsConfig.forEach((bracket) => BRACKETS_PAIR[bracket[0]] = bracket[1]);
+
+  let array = str.split('');
+
+  for(let i = 0; i < array.length; i++) {
+      let currentSymbol = array[i];
+      let topElement = stack[stack.length - 1];
+
+      if(BRACKETS_PAIR[topElement] === currentSymbol) {        
+        stack.pop();        
+      } else if(BRACKETS_PAIR[currentSymbol]) {
+        stack.push(currentSymbol);
+      } else {
+          return false;  
+      };
+      
+  }
+  return stack.length === 0;
 }
